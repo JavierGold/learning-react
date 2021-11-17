832893e80ca1e88d42d2d3eb1fe8ab18d03dedb1
JavierGold <567498jav@gmail.com>
2021-11-16 18:24:28 -0600

    ecomerce 2 Register Page

--- e_commerce2/src/App.jsx
+++ e_commerce2/src/App.jsx
@@ -1,8 +1,9 @@
 import Home from "./pages/Home";
 import Product from "./pages/Product";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <Product/>;
+  return <Register/>;
 };
 
 export default App;
