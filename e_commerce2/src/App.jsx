6cf71a81f33730018950828c4620fcb3ae763be3
JavierGold <567498jav@gmail.com>
2021-11-16 18:46:33 -0600

    ecomerce 2 Front Finalizado con responsive

--- e_commerce2/src/App.jsx
+++ e_commerce2/src/App.jsx
@@ -1,7 +1,7 @@
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
-import Register from "./pages/Register";
+
 
 const App = () => {
   return <Cart/>;
