94b7261762cb8e38cbe5fa2793612dd7039dbc32
JavierGold <567498jav@gmail.com>
2021-11-16 18:28:23 -0600

    ecomerce 2 Cart Page

--- e_commerce2/src/App.jsx
+++ e_commerce2/src/App.jsx
@@ -1,9 +1,10 @@
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 
 const App = () => {
-  return <Register/>;
+  return <Cart/>;
 };
 
 export default App;
