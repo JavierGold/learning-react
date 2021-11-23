a86ad654bc870edc7eb7e898ca46b5e24aaf3274
JavierGold <567498jav@gmail.com>
2021-11-23 17:08:06 -0600

    ecommerce api CRUD user y product

--- e_commerce2Api/routes/auth.js
+++ e_commerce2Api/routes/auth.js
@@ -1,27 +1,28 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: CryptoJS.AES.encrypt(
-        req.body.password,
-        process.env.PASS_SEC
-      ).toString(),
-    });
-  
-    try {
-      const savedUser = await newUser.save();
-      res.status(201).json(savedUser);
-    } catch (err) {
-      res.status(500).json(err);
-    }
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
 
-  //LOGIN
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
 
 router.post('/login', async (req, res) => {
     try{
@@ -46,16 +47,22 @@ router.post('/login', async (req, res) => {
         originalPassword != inputPassword && 
             res.status(401).json("Wrong Password");
 
-        
-        const { password, ...others } = user._doc;  
+        const accessToken = jwt.sign(
+        {
+            id: user._id,
+            isAdmin: user.isAdmin,
+        },
+        process.env.JWT_SEC,
+            {expiresIn:"3d"}
+        );
   
-        res.status(200).json(others);
-
-        
+        const { password, ...others } = user._doc;  
+        res.status(200).json({...others, accessToken});
 
     }catch(err){
         res.status(500).json(err);
     }
 
 });
-module.exports= router
+
+module.exports = router;
