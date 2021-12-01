7c3d7d2823b3546b77520faa9e336217314a9da2
JavierGold <567498jav@gmail.com>
2021-12-01 16:35:53 -0600

    redux auth

--- e_commerce2/src/pages/Login.jsx
+++ e_commerce2/src/pages/Login.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { login } from "../redux/apiCalls";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 
 
 const Container = styled.div`
@@ -58,14 +61,32 @@ const Link = styled.a`
 `;
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    login(dispatch, { username, password });
+  };
   return (
     <Container>
       <Wrapper>
         <Title>SIGN IN</Title>
         <Form>
-          <Input placeholder="username" />
-          <Input placeholder="password" />
-          <Button>LOGIN</Button>
+        <Input
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+           <Input
+            placeholder="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button onClick={handleClick} disabled={isFetching}>
+            LOGIN
+          </Button>
           <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
           <Link>CREATE A NEW ACCOUNT</Link>
         </Form>
